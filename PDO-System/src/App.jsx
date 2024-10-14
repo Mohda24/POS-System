061547a5396878acdabbce528febc9ebd3a3a1bd
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-10-14 21:18:18 +0200

    i added file json have data products and images

--- PDO-System/src/App.jsx
+++ PDO-System/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import products from './data/products.json'
 
 function App() {
+  console.log(products.products[0].image);
+
+
   return (
+
     <div className='bg-red-500'>Hello Lostora</div>
+
+
   )
 }
 
