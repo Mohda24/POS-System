acfe6092965cef5ace09b1d699ac783f3fbc9203
omarchaibb <148008716+omarchaibb@users.noreply.github.com>
2024-10-15 09:03:57 +0200

    fetch data and show it using context

--- PDO-System/src/components/PDO/product/Product.jsx
+++ PDO-System/src/components/PDO/product/Product.jsx
@@ -1,50 +1,33 @@
 import Color from "../color/Color";
 import Sizes from "../sizes/Sizes";
 
-export default function Product() {
-  const sizes = [
-    { id: 1, name: "S" },
-    { id: 2, name: "MD" },
-    { id: 3, name: "XL" },
-    { id: 4, name: "L" },
-    { id: 5, name: "L" },
-  ];
+export default function Product({productName,price,image,sizes,colors}) {
 
-  const colors = [
-    { id: 1, name: "black", colorCode: "#000" },
-    { id: 1, name: "blanc", colorCode: "#fff" },
-    { id: 4, name: "green", colorCode: "#4CAF50" },
-    { id: 5, name: "Bleu foncé", colorCode: "#0D0072" },
-    { id: 7, name: "Marron", colorCode: "#965E47" },
-  ];
 
   return (
     <div className="card flex flex-col gap-[1.25rem] w-[22.1875rem] p-[1.5rem] bg-white rounded-[1.25rem]">
       <div className="flex items-center justify-between gap-[1.25rem]">
+
         {/* image  */}
-        <div
-          style={{ width: "6.875rem", height: "6.25rem", borderRadius: "1rem" }}
-          className="flex-grow-1"
-        >
-          <img
-            src="/src/assets/imgs/produc1.jpg"
-            alt="title"
-            className="w-full h-full rounded-[1rem]"
-          />
+        <div style={{ width: "6.875rem", height: "6.25rem", borderRadius: "1rem" }} className="flex-grow-1" >
+          <img src={"public/images/" + image} alt="title" className="w-full h-full rounded-[1rem]" />
         </div>
+
         {/* title and price  */}
         <div className="flex flex-col gap-[0.625rem]">
-          <h2 className="text-[1.25rem]">Pantalon en Jean Léger</h2>
-          <span className="font-bold text-[1.5rem]">359.00 DH</span>
+          <h2 className="text-[1.25rem]">{productName}</h2>
+          <span className="font-bold text-[1.5rem]">{price} DH</span>
         </div>
       </div>
-      
-      {/* sizes and colors  */}
-      <Sizes sizes={sizes} />
-      <Color colors={colors} />
+
+      {/* conditional rendring if the sizes and colors are available we render them  */}
+      {sizes && sizes.length > 0 ? <Sizes sizes={sizes} /> : ""}
+      {colors && colors.length > 0 ? <Color colors={colors} /> : ""}
+
+      {/* separetor */}
+      <div className="bg-sizes w-full h-[0.09375rem]"></div>
 
       {/* action buttons  */}
-      <div className="bg-sizes w-full h-[0.09375rem]"></div>
       <button className="bg-primary text-white py-[0.4375rem] px-[2.796875rem] rounded-[0.75rem]">
         Ajouter au panier
       </button>
--- PDO-System/src/pages/Products.jsx
+++ PDO-System/src/pages/Products.jsx
@@ -1,12 +1,25 @@
 import Product from "../components/PDO/product/Product";
-
+import { useAppContext } from "../contexts";
 export default function Products() {
+
+  // Get the products from the AppContext
+  const { products } = useAppContext();
+
+ // loop on each product and add it to the Product component and send product information as props
+  const ProductsList =
+    products && products.length > 0
+      ? products.map((product) =>{ 
+      return <Product key={product.id} {...product} />
+    }
+    )
+      : "no products available";
+
+
+      
   return (
     <div className="cards flex items-center flex-wrap gap-[1.875rem]">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      {/* Render the products as a list of cards */}
+      {ProductsList}
     </div>
-  )
+  );
 }
