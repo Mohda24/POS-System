98a9afd8367b80d74920aef733b146c1c5ba1dad
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-10-17 01:15:05 +0200

    I added Part of caise with logic Usestate

--- PDO-System/src/components/PDO/Panel/Panel.jsx
+++ PDO-System/src/components/PDO/Panel/Panel.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ProductCart from '../ProductCart/ProductCart'
 import img from '../../../../public/images/perfum-1.webp'
+import '../../../assets/style.css'
 
 function Panel() {
-    
+
     const [especeValue, setEspece] = React.useState('');
     const [reductionValue, setReduction] = React.useState('');
+    const [caise,setCaise]=React.useState('espece')
 
-    const handleInputChange = (e,setValue) => {
+    const handleInputChange = (e, setValue) => {
         setValue(e.target.value);
     };
 
@@ -29,32 +31,43 @@ function Panel() {
                 <span className='  text-[18px] font-[700] leading-[100%] '>1077.00<span className='text-[12px] ms-[4px]'></span>DH</span>
             </div>
             <div className="Espece relative px-[20px] py-[14px] rounded-[12px] border-[1px] border-[#AAAAAA] flex focus-within:focus">
-                <input 
-                    type="number" 
-                    className='max-w-[calc(100%-87px)] text-[16px] font-[700]' 
+                <input
+                    type="number"
+                    className='max-w-[calc(100%-87px)] text-[16px] font-[700]'
                     value={especeValue}
-                    onChange={(e) => handleInputChange(e,setEspece)}
+                    onChange={(e) => handleInputChange(e, setEspece)}
                     min={0}
                 />
                 <div className="base w-[87px] rounded-[11px] bg-tertiary h-full absolute top-0 right-0 text-[16px] font-[700] flex items-center justify-center">
                     23.00
                 </div>
-                <span className={`placeholder w-[54px] h-[17px] text-[#AAAAAA] bg-white text-[14px] absolute top-[50%] translate-y-[-50%] duration-300 ease-in-out pointer-events-none text-center ${especeValue ? '!top-[0px]' :''}`}>Espèce</span>
+                <span className={`placeholder w-[54px] h-[17px] text-[#AAAAAA] bg-white text-[14px] absolute top-[50%] translate-y-[-50%] duration-300 ease-in-out pointer-events-none text-center ${especeValue ? '!top-[0px]' : ''}`}>Espèce</span>
             </div>
             {/* redection */}
             <div className="Redection relative px-[20px] py-[14px] rounded-[12px] border-[1px] border-[#AAAAAA] flex focus-within:focus">
-                <input 
-                    type="number" 
-                    className='max-w-full w-full text-[16px] font-[700]' 
+                <input
+                    type="number"
+                    className='max-w-full w-full text-[16px] font-[700]'
                     value={reductionValue}
-                    onChange={(e) => handleInputChange(e,setReduction)}
+                    onChange={(e) => handleInputChange(e, setReduction)}
                     min={0}
                 />
 
-                <span className={`placeholder w-[71px] h-[17px] text-[#AAAAAA] bg-white text-[14px] absolute top-[50%] translate-y-[-50%] duration-300 ease-in-out pointer-events-none text-center ${reductionValue ? '!top-[0px]' :''}`}>Réduction</span>
+                <span className={`placeholder w-[71px] h-[17px] text-[#AAAAAA] bg-white text-[14px] absolute top-[50%] translate-y-[-50%] duration-300 ease-in-out pointer-events-none text-center ${reductionValue ? '!top-[0px]' : ''}`}>Réduction</span>
             </div>
             {/* Caisse */}
-            
+            <div className="caise flex gap-[10px]">
+                <div onClick={()=>setCaise('espece')} className={`p-[10px] border-2 rounded-[16px] flex flex-col gap-[4px] items-center w-full cursor-pointer select-none ${caise==='espece' ? 'bg-white border-2 border-black' :'bg-tertiary border-none'} `}>
+                    <div className={`icon-banknote `} />
+                    <span className='text-[16px]'>Espèce</span>
+
+                </div>
+                <div onClick={()=>setCaise('carte')} className={`p-[10px] border-2 rounded-[16px] flex flex-col gap-[4px] items-center w-full cursor-pointer select-none ${caise==='carte' ? 'bg-white border-2 border-black' :'bg-tertiary border-none'} `}>
+                    <div className='icon-creditcards' />
+                    <span className='text-[16px]'>Carte</span>
+                </div>
+                <button className='w-full py-[14px] px-[20px] bg-primary rounded-[16px] text-[16px] text-white font-[700]'>Valider</button>
+            </div>
         </section>
     )
 }
