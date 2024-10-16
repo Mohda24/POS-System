30611b49be9b37ec67f64e388476b7c88ab27931
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-10-17 00:50:27 +0200

    I added Part of Redection and handle it

--- PDO-System/src/components/PDO/Panel/Panel.jsx
+++ PDO-System/src/components/PDO/Panel/Panel.jsx
@@ -4,10 +4,11 @@ import img from '../../../../public/images/perfum-1.webp'
 
 function Panel() {
     
-    const [inputValue, setInputValue] = React.useState('');
+    const [especeValue, setEspece] = React.useState('');
+    const [reductionValue, setReduction] = React.useState('');
 
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
+    const handleInputChange = (e,setValue) => {
+        setValue(e.target.value);
     };
 
     return (
@@ -31,14 +32,29 @@ function Panel() {
                 <input 
                     type="number" 
                     className='max-w-[calc(100%-87px)] text-[16px] font-[700]' 
-                    value={inputValue}
-                    onChange={handleInputChange}
+                    value={especeValue}
+                    onChange={(e) => handleInputChange(e,setEspece)}
+                    min={0}
                 />
                 <div className="base w-[87px] rounded-[11px] bg-tertiary h-full absolute top-0 right-0 text-[16px] font-[700] flex items-center justify-center">
                     23.00
                 </div>
-                <span className={`placeholder w-[54px] h-[17px] text-[#AAAAAA] bg-white text-[14px] absolute top-[50%] translate-y-[-50%] duration-300 ease-in-out pointer-events-none text-center ${inputValue ? 'top-[0px]' : ''}`}>Espèce</span>
+                <span className={`placeholder w-[54px] h-[17px] text-[#AAAAAA] bg-white text-[14px] absolute top-[50%] translate-y-[-50%] duration-300 ease-in-out pointer-events-none text-center ${especeValue ? '!top-[0px]' :''}`}>Espèce</span>
             </div>
+            {/* redection */}
+            <div className="Redection relative px-[20px] py-[14px] rounded-[12px] border-[1px] border-[#AAAAAA] flex focus-within:focus">
+                <input 
+                    type="number" 
+                    className='max-w-full w-full text-[16px] font-[700]' 
+                    value={reductionValue}
+                    onChange={(e) => handleInputChange(e,setReduction)}
+                    min={0}
+                />
+
+                <span className={`placeholder w-[71px] h-[17px] text-[#AAAAAA] bg-white text-[14px] absolute top-[50%] translate-y-[-50%] duration-300 ease-in-out pointer-events-none text-center ${reductionValue ? '!top-[0px]' :''}`}>Réduction</span>
+            </div>
+            {/* Caisse */}
+            
         </section>
     )
 }
