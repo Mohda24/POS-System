690a369f0ebdd43161f63472988b4cc2b17f7b24
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-10-16 22:46:59 +0200

    i added part of products in cart and total price

--- PDO-System/src/components/PDO/Panel/Panel.jsx
+++ PDO-System/src/components/PDO/Panel/Panel.jsx
@@ -16,8 +16,17 @@ function Panel() {
                     <ProductCart productName='Pantalon en Jean Léger' productPrice='359.99' productImage={img} />
                 </div>
             </div>
-            <div className="total">
-                <h3 className='text-[18px]'>Total</h3>
+            <div className="total flex justify-between items-center">
+                <h3 className='text-[18px] font-[700] leading-[100%]'>Total</h3>
+                <span className='  opacity-[0.4] text-[18px] font-[700] leading-[100%] line-through'>1077.00<span className='text-[12px] ms-[4px]'></span>DH</span>
+                <span className='  text-[18px] font-[700] leading-[100%] '>1077.00<span className='text-[12px] ms-[4px]'></span>DH</span>
+            </div>
+            <div className="Espece relative px-[20px] py-[14px] rounded-[12px] border-[1px] border-[#AAAAAA] flex">
+                <input type="number" />
+                <div className="base w-[87px] rounded-[11px] bg-tertiary h-full absolute top-0 right-0 text-[16px] font-[700] flex items-center justify-center">
+                    23.00
+                </div>
+
             </div>
         </section>
     )
